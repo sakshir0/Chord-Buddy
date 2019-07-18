9d5249cc01d099c801c1162e3f1244fda46a1407
sakshir0 <32418305+sakshir0@users.noreply.github.com>
2019-07-17 21:33:16 -0400

    Changed image neural network code to be more readable and simplified.

--- userPart.py
+++ userPart.py
@@ -1,15 +1,25 @@
 from keras.models import load_model
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
 import cv2
 import numpy as np
 
-# load  model
+#load model
 model = load_model("model.h5")
 
-#load image from file
-#img = cv2.imread('validation/cMajor/cMajor1.jpg')
+img = ['validation/cMajor/cMajor1.jpg']
+X, y = read_and_process_image(img)
+x = np.array(X)
+test_datagen = ImageDataGenerator(rescale=1./255)
+for batch in test_datagen.flow(x, batch_size=1):
+	pred = model.predict(batch)
+#test_datagen = ImageDataGenerator(rescale=1./255)
+#for batch in test_datagen
+print(X[0])
+print(y)
 
+'''
 def load_image(image_path, grayscale=False, target_size=None):
     color_mode = 'grayscale'
     if grayscale == False:
@@ -24,4 +34,5 @@ img = load_image('validation/cMajor/cMajor1.jpg',False,(150, 150))
 x = img_to_array(img)
 prediction = model.predict(x.reshape((1,3,150, 150)),batch_size=1, verbose=0)
 print(prediction)
+'''
 
